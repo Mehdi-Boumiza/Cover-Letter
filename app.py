6b460baddddc11d53d0626c7db9263e55cef977e
Mehdi-Boumiza <Mehdi.boumizaa@gmail.com>
2025-06-12 20:55:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ scope = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/drive"]
 
 load_dotenv()
-service_account_info = json.loads(os.getenv("credentials"))
-groq_api_key = os.getenv("GROQ_API_KEY")
-creds = Credentials.from_service_account_info(service_account_info,scopes = scope)
+service_account_info = st.secrets["credentials"]
+groq_api_key = st.secrets("GROQ_API_KEY")
+creds = service_account.Credentials.from_service_account_info(service_account_info,scopes = scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("cover").sheet1
